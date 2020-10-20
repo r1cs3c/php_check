6a718a9fc6456ae529d23b4a6c3bedbeae0f869a
Ric Campo (OSCP) <16293986+r1cs3c@users.noreply.github.com>
2020-10-20 11:52:21 +1100

    Create testphp.php
    php vulnerable code from https://resources.infosecinstitute.com/php-lab-analyze-code-spot-vulnerability/

---
+++ testphp.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<form action=””/>
+
+<input type=”text” name=”id” />
+
+<input type=”submit” />
+
+</form>
+
+</br>
+
+</body>
+
+</html>
+
+<?php
+
+$servername = “localhost”;
+
+$username = “root”;
+
+$password = “toor”;
+
+$dbname = “infosec”;
+
+// Create connection
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+
+if (!$conn) {
+
+die(“Connection failed: ” . mysqli_connect_error());
+
+}
+
+$id = $_GET[‘id’];
+
+$sql = “SELECT * FROM products where ID=”.$id;
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+
+// output data of each row
+
+while($row = mysqli_fetch_assoc($result)) {
+
+echo “ID: ” . $row[“ID”]. ” – Name: ” . $row[“name”]. ” – Price: ” . $row[“price”]. “<br>”;
+
+}
+
+} else {
+
+echo “0 results”;
+
+}
+
+mysqli_close($conn);
+
+?>
